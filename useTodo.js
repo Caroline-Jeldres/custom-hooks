bb6bf2f2df78f53cf6dfdaa5a68ca59469fdc6c3
Caroline Jeldres <cjeldresm@gmail.com>
2024-08-17 17:00:38 -0400

    primer commit

---
+++ useTodo.js
@@ -0,0 +1,48 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodo = () => {
+  const [todos, dispatchTodo] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    dispatchTodo({
+      type: "ADD",
+      payload: todo,
+    });
+  };
+
+  const handleRemoveTodo = (id) => {
+    dispatchTodo({
+      type: "REMOVE",
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatchTodo({
+      type: "TOGGLE",
+      payload: id,
+    });
+  };
+
+  const todoCounter = todos.length;
+
+  const pendingTodoCounter = todos.filter((todo) => !todo.done).length;
+
+  return {
+    todos,
+    handleNewTodo,
+    handleRemoveTodo,
+    handleToggleTodo,
+    todoCounter,
+    pendingTodoCounter,
+  };
+};
